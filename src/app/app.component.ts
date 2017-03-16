ad0a58c98cee56f85112ca22b622a41629095b96
Jorge Jimenez <george@shocklogic.com>
2017-03-16 17:54:10 -0400

    Added update and delete functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,17 @@ export class AppComponent {
   appState: string;
   activeKey: string;
 
+  activeCompany: string; 
+  activeCategory:string;
+  activeYearsInBusiness: string;
+  activeDescription: string;
+  activePhone: string;
+  activeEmail: string;
+  activeStreetAddress: string;
+  activeCity: string;
+  activeState: string;
+  activeZipcode: string;
+
   constructor(private _firebaseService: FirebaseService) {}
 
   ngOnInit() {
@@ -56,7 +67,7 @@ export class AppComponent {
 
     var newBusiness = {
       company: company, 
-      category:category,
+      category: category,
       years_in_business: years_in_business,
       description: description,
       phone: phone,
@@ -69,6 +80,43 @@ export class AppComponent {
     }
 
     this._firebaseService.addBusiness(newBusiness);
+    this.changeState('default');
+  }
+
+  showEdit(business){
+    this.changeState('edit', business.$key);
+    this.activeCompany = business.company;   
+    this.activeCategory = business.category;
+    this.activeYearsInBusiness = business.years_in_business;
+    this.activeDescription = business.description;
+    this.activePhone = business.phone;
+    this.activeEmail = business.email;
+    this.activeStreetAddress = business.street_address;
+    this.activeCity = business.city;
+    this.activeState = business.state;
+    this.activeZipcode = business.zipcode;
+  }
+
+  updateBusiness(){
+    var updBusiness = {
+      company: this.activeCompany, 
+      category: this.activeCategory,
+      years_in_business: this.activeYearsInBusiness,
+      description: this.activeDescription,
+      phone: this.activePhone,
+      email: this.activeEmail,
+      street_address: this.activeStreetAddress,
+      city: this.activeCity,
+      state: this.activeState,
+      zipcode: this.activeZipcode
+    }
+
+    this._firebaseService.updateBusiness(this.activeKey, updBusiness);
+    this.changeState('default');
+  }
+
+  deleteBusiness(key) {
+    this._firebaseService.deleteBusiness(key);
     this.changeState('default');
   }
 
--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -34,4 +34,13 @@ export class FirebaseService {
   addBusiness(newBusiness) {
     return this.businesses.push(newBusiness);
   }
+
+  updateBusiness(key, updBusiness) {
+    return this.businesses.update(key, updBusiness);
+  }
+
+  deleteBusiness(key) {
+    return this.businesses.remove(key);
+  }
+  
 }
